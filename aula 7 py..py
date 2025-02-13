4117e405dea2049ccfe35e8e2f7d9d5617223fac
Ramon1858 <caetanoramon678@gmail.com>
2025-02-13 17:39:16 -0300

    Add files via upload

---
+++ aula 7 py..py
@@ -0,0 +1,26 @@
+# EXERCICIO 1
+#pessoas = [1,2,3,4,5]
+#print (pessoas)
+
+#EXERCICIO 0
+#lista = range(2,21,2)
+#print(lista)
+
+#eXERCICIO 2
+#numeros = [1,2,3,4,5,6,7,8,9,0,10,11,12,13,14,15,16,17]
+#print(numeros[2])
+
+#EXERCICIO 3
+#numeros1 = [1,2,3,4,5,6,7,8,9,0,10)
+#numeros1.insert(8,9)
+#print(numeros1)
+
+#EXERCICIO 4
+#numeros = [1,2,3,4,5,6,7,8,9,0,10]
+#numeros.remove(5)
+#print(numeros)
+
+#EXERCICIO 5
+#carros =['ferrari','sandero','mercedes']
+#numeros2 = [1,2,3,4,5]
+#print(carros+numeros2)
